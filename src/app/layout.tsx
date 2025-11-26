b71d152f0b02d18afdd1652c2b52c9e682605441
Arnab Chakraborty <that.zytact@gmail.com>
2025-11-26 19:51:45 +0530

    change default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
             >
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="dark"
+                    defaultTheme="system"
                     enableSystem
                     disableTransitionOnChange={false}
                 >
