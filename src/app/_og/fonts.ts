4670a95605fe2fb5a74cba92974c04978426ae11
Arnab Chakraborty <that.zytact@gmail.com>
2026-05-12 16:05:33 +0530

    use local fonts

--- src/app/_og/fonts.ts
+++ src/app/_og/fonts.ts
@@ -1,18 +1,29 @@
-const instrumentSerifUrl =
-    'https://raw.githubusercontent.com/google/fonts/main/ofl/instrumentserif/InstrumentSerif-Regular.ttf';
-const dmSansUrl =
-    'https://raw.githubusercontent.com/googlefonts/dm-fonts/main/Sans/fonts/ttf/DMSans-Regular.ttf';
-const caveatUrl =
-    'https://raw.githubusercontent.com/googlefonts/caveat/master/fonts/ttf/Caveat-Bold.ttf';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+
+type OgFontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+type OgFontStyle = 'normal' | 'italic';
+type OgFont = {
+    data: Buffer | ArrayBuffer;
+    name: string;
+    weight?: OgFontWeight;
+    style?: OgFontStyle;
+    lang?: string;
+};
+
+const fontsDir = path.join(process.cwd(), 'public', 'fonts');
+const instrumentSerifPath = path.join(fontsDir, 'InstrumentSerif-Regular.ttf');
+const dmSansPath = path.join(fontsDir, 'DMSans-Regular.ttf');
+const caveatPath = path.join(fontsDir, 'Caveat-Bold.ttf');
 
 export async function loadOgFonts() {
     const [serif, sans, hand] = await Promise.all([
-        fetch(instrumentSerifUrl).then((response) => response.arrayBuffer()),
-        fetch(dmSansUrl).then((response) => response.arrayBuffer()),
-        fetch(caveatUrl).then((response) => response.arrayBuffer()),
+        readFile(instrumentSerifPath),
+        readFile(dmSansPath),
+        readFile(caveatPath),
     ]);
 
-    return [
+    const fonts: OgFont[] = [
         {
             name: 'Instrument Serif',
             data: serif,
@@ -32,4 +43,6 @@ export async function loadOgFonts() {
             style: 'normal',
         },
     ];
+
+    return fonts;
 }
